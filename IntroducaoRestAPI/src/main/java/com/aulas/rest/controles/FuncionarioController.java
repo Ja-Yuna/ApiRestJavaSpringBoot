453b28fdf02d329a239fda0203691f2250dd329a
Raphaela <raphaela.nunes@capgemini.com>
2023-12-01 21:47:21 -0300

    melhorias

---
+++ IntroducaoRestAPI/src/main/java/com/aulas/rest/controles/FuncionarioController.java
@@ -0,0 +1,12 @@
+package com.aulas.rest.controles;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/funcionario")
+public class FuncionarioController {
+	
+	
+
+}
